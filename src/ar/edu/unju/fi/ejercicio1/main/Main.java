a50160a161ca44b09253b69e0449d99186361edf
mathy <matiwordgit@gmail.com>
2024-04-25 20:26:22 -0300

    Ejercicio1

--- src/ar/edu/unju/fi/ejercicio1/main/Main.java
+++ src/ar/edu/unju/fi/ejercicio1/main/Main.java
@@ -1,9 +1,30 @@
 package ar.edu.unju.fi.ejercicio1.main;
 
+import java.util.ArrayList;
+
+import ar.edu.unju.fi.ejercicio1.model.Producto;
+import ar.edu.unju.fi.ejercicio1.model.Producto.Categoria;
+import ar.edu.unju.fi.ejercicio1.model.Producto.OrigenFabricacion;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// Rama ejercicio01
+		
+		ArrayList <Producto> producto_s = new ArrayList<Producto>();
+		
+		Producto producto1 = new Producto( OrigenFabricacion.ARGENTINA,Categoria.ELECTROHOGAR);
+		
+		producto1.setCodigo("111");
+		producto1.setDescripcion("Lavarropa");
+		producto1.setPrecioUnitario(100000);
+		
+		producto_s.add(producto1);
+		
+		System.out.println(producto_s.get(0).toString());
+		
+		
+		
+		
 	}
 
 }
